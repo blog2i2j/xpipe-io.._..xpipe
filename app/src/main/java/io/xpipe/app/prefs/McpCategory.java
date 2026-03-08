db74f608f1a81853de92acda124238c93ff18c2d
crschnick <crschnick@xpipe.io>
2026-03-08 17:04:18 +0000

    Fixes for claude code mcp

--- app/src/main/java/io/xpipe/app/prefs/McpCategory.java
+++ app/src/main/java/io/xpipe/app/prefs/McpCategory.java
@@ -88,6 +88,10 @@ public class McpCategory extends AppPrefsCategory {
                }
                """);
 
+        var claudeCodeTemplate = createMcpConfig("""
+               $ claude mcp add %s --transport http "http://localhost:%s/mcp" --header "Authorization: Bearer %s"
+               """);
+
         var tabComp = RegionBuilder.of(() -> {
             var vsCode = new TextArea();
             vsCode.setEditable(false);
@@ -118,10 +122,10 @@ public class McpCategory extends AppPrefsCategory {
 
             var claude = new TextArea();
             claude.setEditable(false);
-            claude.textProperty().bind(vsCodeTemplate);
+            claude.textProperty().bind(claudeCodeTemplate);
             claude.setPrefRowCount(12);
             var claudeTab = new Tab();
-            claudeTab.textProperty().bind(AppI18n.observable("claude"));
+            claudeTab.textProperty().bind(AppI18n.observable("claudeCode"));
             claudeTab.setContent(claude);
             claudeTab.setClosable(false);
 
